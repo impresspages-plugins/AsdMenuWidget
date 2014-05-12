76a69da4963bac126f1d9ca6ce3e90d2c9c9aac2
Work <Work@Work-PC>
2014-05-12 15:01:16 +0300

    Minor update.

---
+++ Widget/Menu/Filter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugin\AsdMenuWidget\Widget\Menu;
+
+
+class Filter {
+    public static function ipWidgetManagementMenu($optionsMenu, $widgetRecord)
+    {
+        echo 'test';
+        $optionsMenu = array();
+        return $optionsMenu;
+    }
+}
--- Widget/Menu/skin/default.php
+++ Widget/Menu/skin/default.php
@@ -1,6 +1,6 @@
 <?php if( !empty( $data ) ): ?>
     <?php 
         $data['items'] = \Ip\Menu\Helper::getMenuItems( $data['menu']['name'] );
-        echo ipView('menu.php', array( 'data' => $data, 'depth' => 1 ))->render(); 
+        echo ipView('view/menu.php', array( 'data' => $data, 'depth' => 1 ))->render(); 
     ?>
 <?php endif; ?>
