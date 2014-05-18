b23529a030377dba925950feaa85dc4c2b50aa61
Simonas Dimavičius <asd@asd.lt>
2014-05-18 14:47:41 +0300

    Updated query to output all pages which has childrens.

--- Widget/Menu/Model.php
+++ Widget/Menu/Model.php
@@ -1,22 +1,49 @@
 <?php
-/**
- * @package   ImpressPages
- */
 
 namespace Plugin\AsdMenuWidget\Widget\Menu;
 
 
 class Model
 {
-    public static function getTopMenusList()
+    public static function getMenusList()
     {
-        $languageCode = ipContent()->getCurrentLanguage()->code;
+        if( ipGetOption('AsdMenuWidget.multilanguage') == 'Yes' ) {
+            $multilanguage = null;
+        } else {
+            $languageCode = ipContent()->getCurrentLanguage()->code;
+            $multilanguage = "`languageCode` = '$languageCode' AND";
+        }
+        
         $table = ipTable('page');
-        $sql = "SELECT `title`, `alias`, `languageCode` FROM $table WHERE `parentId` = 0 ORDER BY `languageCode` ASC, `title` ASC";
+        $sql = "SELECT `title`, `alias`, `languageCode`, `parentId`, `id` FROM $table WHERE $multilanguage `isVisible` = 1 AND `isDeleted` = 0 ORDER BY `languageCode` ASC, `title` ASC";
         $results = ipDb()->fetchAll( $sql );
         foreach( $results as $result ) {
-            $returnData[] = array( $result['alias'], $result['languageCode'] . ' - ' . $result['title'] );
+            $allPages[$result['parentId']][$result['id']] = $result;
         }
+
+        $returnData = makeMenu( 0, $allPages, 0, $multilanguage );
+        
         return $returnData;
     }
+}
+
+function makeMenu( $target, $allPages, $level, $multilanguage ) {
+    $return = array();
+    if( !empty( $allPages[$target] ) ) {
+        foreach( $allPages[$target] as $id => $page ) {
+            if( !empty( $allPages[$id] ) ) {
+                $title =  str_repeat( "__", $level) . $page['title'];
+                if( empty( $multilanguage ) ) {
+                    $title  = "{$page['languageCode']}: {$title}";
+                }
+                if( empty( $page['alias'] ) ) {
+                    $return[] = array( false, $title . ' (w/o Alias)' );
+                } else {
+                    $return[] = array( $page['alias'], $title );
+                }
+                $return = array_merge( $return, makeMenu( $id, $allPages, $level+1, $multilanguage ) );
+            }
+        }
+    }
+    return $return;
 }
